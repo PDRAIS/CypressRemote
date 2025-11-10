87bdf07571edaea484cf95d3508a18e30b135c83
samGreenwood2022 <120378330+samGreenwood2022@users.noreply.github.com>
2025-11-10 15:09:45 +0000

    Refactor test descriptions and enhance telephone number verification in source regression tests

--- cypress/e2e/1-getting-started/first-test-sg.cy.js
+++ cypress/e2e/1-getting-started/first-test-sg.cy.js
@@ -1,21 +1,36 @@
 describe("Source Regression Tests", () => {
   // first Test - navigate to dyson manufacturer homepage on nbs source
-    it("navigate to NBS Source", () => {
+  it("navigate to NBS Source", () => {
     cy.visit("https://source.thenbs.com");
+    // cy.get("#onetrust-accept-btn-handler").click();
+    cy.contains('button', 'Accept All Cookies').click();
     cy.get("#mat-input-0").click();
     cy.get("#mat-input-0").type("Dyson");
-    cy.get("#cdk-overlay-0 a.truncate").click();
-    cy.url().should(
+    cy.get("#cdk-overlay-0 a.truncate", { timeout: 10000 }).click();
+    cy.url({ timeout: 10000 }).should(
       "include",
       "https://source.thenbs.com/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview"
     );
   });
 
-  it("name of second test", () => {
-    // test steps for the second test
+  it("verify the telephone number is correct with expected href", () => {
+    // Navigate to the page and accept cookies
+    cy.visit("https://source.thenbs.com");
+    // cy.contains('button', 'Accept All Cookies').debug().click();
+    cy.contains('button', 'Accept All Cookies').click();
+    cy.get("#mat-input-0").click();
+    cy.get("#mat-input-0").type("Dyson");
+    cy.get("#cdk-overlay-0 a.truncate", { timeout: 10000 }).click();
+    cy.url({ timeout: 10000 }).should(
+      "include",
+      "https://source.thenbs.com/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview"
+    );
 
-    
+    // Verify the telephone link element
+    cy.contains('a', '08003457788', { timeout: 10000 })
+      .should('be.visible')
+      .should('have.attr', 'href', 'tel:08003457788')
+      .should('have.attr', 'title', 'Call 08003457788')
+      .and('have.attr', 'action', 'telephone');
   });
-
-
 });
