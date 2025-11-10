ca042bf6743f9d4c09c43d802f2166867d72c29f
samGreenwood2022 <120378330+samGreenwood2022@users.noreply.github.com>
2025-11-10 15:39:19 +0000

    Added third test - verify nbs source logo href

--- cypress/e2e/1-getting-started/first-test-sg.cy.js
+++ cypress/e2e/1-getting-started/first-test-sg.cy.js
@@ -3,7 +3,7 @@ describe("Source Regression Tests", () => {
   it("navigate to NBS Source", () => {
     cy.visit("https://source.thenbs.com");
     // cy.get("#onetrust-accept-btn-handler").click();
-    cy.contains('button', 'Accept All Cookies').click();
+    cy.contains("button", "Accept All Cookies").click();
     cy.get("#mat-input-0").click();
     cy.get("#mat-input-0").type("Dyson");
     cy.get("#cdk-overlay-0 a.truncate", { timeout: 10000 }).click();
@@ -17,7 +17,7 @@ describe("Source Regression Tests", () => {
     // Navigate to the page and accept cookies
     cy.visit("https://source.thenbs.com");
     // cy.contains('button', 'Accept All Cookies').debug().click();
-    cy.contains('button', 'Accept All Cookies').click();
+    cy.contains("button", "Accept All Cookies").click();
     cy.get("#mat-input-0").click();
     cy.get("#mat-input-0").type("Dyson");
     cy.get("#cdk-overlay-0 a.truncate", { timeout: 10000 }).click();
@@ -27,10 +27,47 @@ describe("Source Regression Tests", () => {
     );
 
     // Verify the telephone link element
-    cy.contains('a', '08003457788', { timeout: 10000 })
-      .should('be.visible')
-      .should('have.attr', 'href', 'tel:08003457788')
-      .should('have.attr', 'title', 'Call 08003457788')
-      .and('have.attr', 'action', 'telephone');
+    cy.contains("a", "08003457788", { timeout: 10000 })
+      .should("be.visible")
+      .should("have.attr", "href", "tel:08003457788")
+      .should("have.attr", "title", "Call 08003457788")
+      .and("have.attr", "action", "telephone");
+  });
+
+  it("verify h1 value is correct", () => {
+    cy.visit("https://source.thenbs.com");
+    // cy.get("#onetrust-accept-btn-handler").click();
+    cy.contains("button", "Accept All Cookies").click();
+    cy.get("#mat-input-0").click();
+    cy.get("#mat-input-0").type("Dyson");
+    cy.get("#cdk-overlay-0 a.truncate", { timeout: 10000 }).click();
+    cy.url({ timeout: 10000 }).should(
+      "include",
+      "https://source.thenbs.com/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview"
+    );
+    // verify the h1 value shows correct text
+    cy.get("h1")
+      .should("be.visible")
+      .should("contain.text", "Dyson");
+  });
+
+  it("verify NBS Source logo href is correct", () => {
+    cy.visit("https://source.thenbs.com");
+    cy.contains("button", "Accept All Cookies").click();
+    cy.get("#mat-input-0").click();
+    cy.get("#mat-input-0").type("Dyson");
+    cy.get("#cdk-overlay-0 a.truncate", { timeout: 10000 }).click();
+    cy.url({ timeout: 10000 }).should(
+      "include",
+      "https://source.thenbs.com/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview"
+    );
+    
+    // Verify the NBS Source logo link has correct href
+    cy.contains('NBS Source')
+      .should("be.visible")
+      .should("have.attr", "href", "/")
+      .should("have.class", "brand-primary")
+      .should("have.class", "wrapper")
+      .should("contain", "NBS Source");
   });
 });
