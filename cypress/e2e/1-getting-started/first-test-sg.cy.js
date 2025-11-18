581946fae3516990167188e3ec70702f2a38d363
PDRAIS <aclt1976@gmail.com>
2025-11-18 20:28:39 +0000

    Added second test that confirms the phone number

--- cypress/e2e/1-getting-started/first-test-sg.cy.js
+++ cypress/e2e/1-getting-started/first-test-sg.cy.js
@@ -1,21 +1,32 @@
 describe("Source Regression Tests", () => {
-  // first Test - navigate to dyson manufacturer homepage on nbs source
-    it("navigate to NBS Source", () => {
+
+  // first Test - navigate to dyson manufacturer homepage on NBS Source
+  it("navigate to NBS Source", () => {
     cy.visit("https://source.thenbs.com");
     cy.get("#mat-input-0").click();
     cy.get("#mat-input-0").type("Dyson");
     cy.get("#cdk-overlay-0 a.truncate").click();
+
     cy.url().should(
       "include",
       "https://source.thenbs.com/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview"
     );
   });
 
-  it("name of second test", () => {
-    // test steps for the second test
+  it("verify the telephone link has the correct number and href", () => {
+    cy.visit("https://source.thenbs.com");
+    cy.get("#mat-input-0").click();
+    cy.get("#mat-input-0").type("Dyson");
+    cy.get("#cdk-overlay-0 a.truncate").click();
 
-    
+    cy.url().should(
+      "include",
+      "https://source.thenbs.com/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview"
+    );
+
+    cy.contains("a", "08003457788")
+      .should("have.attr", "href", "tel:08003457788")
+      .and("be.visible");
   });
-
 
 });
