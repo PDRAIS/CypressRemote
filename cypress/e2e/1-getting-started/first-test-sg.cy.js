85fe3c882212d6e7256548364ea8110a52fde8cb
Sam Greenwood <sam_greenwood26@hotmail.com>
2026-02-03 19:03:16 +0000

    Refactor source regression tests for improved readability and structure

--- cypress/e2e/1-getting-started/first-test-sg.cy.js
+++ cypress/e2e/1-getting-started/first-test-sg.cy.js
@@ -1,5 +1,4 @@
 describe("Source Regression Tests", () => {
-
   // first Test - navigate to dyson manufacturer homepage on NBS Source
   it("navigate to NBS Source", () => {
     cy.visit("https://source.thenbs.com");
@@ -9,7 +8,7 @@ describe("Source Regression Tests", () => {
 
     cy.url().should(
       "include",
-      "https://source.thenbs.com/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview"
+      "https://source.thenbs.com/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview",
     );
   });
 
@@ -21,7 +20,7 @@ describe("Source Regression Tests", () => {
 
     cy.url().should(
       "include",
-      "https://source.thenbs.com/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview"
+      "https://source.thenbs.com/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview",
     );
 
     cy.contains("a", "08003457788")
@@ -37,37 +36,77 @@ describe("Source Regression Tests", () => {
 
     cy.url().should(
       "include",
-      "https://source.thenbs.com/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview"
+      "https://source.thenbs.com/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview",
     );
-    cy.get('h1')
-      .should('exist')
-      .and('be.visible')
-      .and('have.text', 'Dyson');
-
-
-
-
+    cy.get("h1").should("exist").and("be.visible").and("have.text", "Dyson");
   });
-
 
   it("Confirm that the logo  Href attribute present ", () => {
     cy.visit("https://source.thenbs.com");
     cy.get("#mat-input-0").click();
     cy.get("#mat-input-0").type("Dyson");
     cy.get("#cdk-overlay-0 a.truncate").click();
-    
+
     cy.url().should(
       "include",
-      "https://source.thenbs.com/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview"
+      "https://source.thenbs.com/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview",
     );
-    
+
     cy.contains("a", "08003457788")
       .should("have.attr", "href", "tel:08003457788")
       .and("be.visible");
-    cy.get('a[action="manufacturer-header-link"]').should('be.visible');
-    cy.get('a[action="manufacturer-header-link"]').should('have.text', ' I\'m a manufacturer ');
+    cy.get('a[action="manufacturer-header-link"]').should("be.visible");
+    cy.get('a[action="manufacturer-header-link"]').should(
+      "have.text",
+      " I'm a manufacturer ",
+    );
   });
 
+  it("Confirm manufacturer homepage tabs are visible, in the correct order, have the correct text & href ", () => {
+    const tabs = [
+      {
+        selector: '[data-cy="overviewTab"]',
+        text: "Overview",
+        href: "/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview",
+      },
+      {
+        selector: '[data-cy="productsTab"]',
+        text: "Products",
+        href: "/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/products",
+      },
+      {
+        selector: '[data-cy="certificatesTab"]',
+        text: "Certifications",
+        href: "/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/third-party-certifications",
+      },
+      {
+        selector: '[data-cy="literatureTab"]',
+        text: "Literature",
+        href: "/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/literature",
+      },
+      {
+        selector: '[data-cy="caseStudiesTab"]',
+        text: "Case studies",
+        href: "/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/case-studies",
+      },
+      {
+        selector: '[data-cy="aboutTab"]',
+        text: "About us",
+        href: "/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/about",
+      },
+    ];
+
+    // Verify each tab exists, is visible, has correct text and href
+    tabs.forEach((tab) => {
+      cy.get(tab.selector)
+        .should("be.visible")
+        .and("contain.text", tab.text)
+        .and("have.attr", "href", tab.href);
+    });
+
+    // Verify correct order
+    cy.get("nav a[mat-tab-link]").each(($el, index) => {
+      cy.wrap($el).should("contain.text", tabs[index].text);
+    });
+  });
 });
-
-
