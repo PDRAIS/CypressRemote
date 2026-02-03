54eaa1e50493656b9b2cce18d4c301e02d5cd7be
PDRAIS <aclt1976@gmail.com>
2026-02-03 18:48:26 +0000

    New wordl Designs demo page test

---
+++ cypress/e2e/newworlddesigns/new.cy.js
@@ -0,0 +1,13 @@
+describe("Source Regression Tests", () => {
+  // first Test - navigate to dyson manufacturer homepage on NBS Source
+  it("navigate to NBS Source", () => {
+    cy.visit("https://www.newworlddesigns.co.uk/");
+    cy.url().should(
+      "include",
+      "https://www.newworlddesigns.co.uk/"
+    );
+    cy.get('#content a.dt-btn span').should('be.visible');
+    cy.get('#content div.vc_custom_1712244435327 h2').click();
+  });
+
+  
