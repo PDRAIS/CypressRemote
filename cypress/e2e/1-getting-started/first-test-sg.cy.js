95c88a6fd01d8ae873472d958fd254e7be2d084b
Sam Greenwood <sam_greenwood26@hotmail.com>
2026-02-03 19:10:28 +0000

    Add visit and URL verification for manufacturer homepage tabs test

--- cypress/e2e/1-getting-started/first-test-sg.cy.js
+++ cypress/e2e/1-getting-started/first-test-sg.cy.js
@@ -63,6 +63,16 @@ describe("Source Regression Tests", () => {
   });
 
   it("Confirm manufacturer homepage tabs are visible, in the correct order, have the correct text & href ", () => {
+    cy.visit("https://source.thenbs.com");
+    cy.get("#mat-input-0").click();
+    cy.get("#mat-input-0").type("Dyson");
+    cy.get("#cdk-overlay-0 a.truncate").click();
+
+    cy.url().should(
+      "include",
+      "https://source.thenbs.com/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview",
+    );
+
     const tabs = [
       {
         selector: '[data-cy="overviewTab"]',
