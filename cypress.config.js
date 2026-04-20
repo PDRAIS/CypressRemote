880734b11f19c4fe2546f9a7d4a0b0b93c795048
Sam Greenwood <sam_greenwood26@hotmail.com>
2026-04-20 20:22:22 +0100

    feat: integrate Cucumber preprocessor and update configuration for step definitions

--- cypress.config.js
+++ cypress.config.js
@@ -5,20 +5,30 @@ const {
 const path = require("path");
 const fs = require("fs");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
   projectId: "jrhhew",
   e2e: {
+    specPattern: "cypress/e2e/**/*.{cy.js,feature}",
     // NOTE: Storing secrets directly in this file is insecure for shared repositories.
     // Prefer using CI/CD secret storage or environment variables in production.
     env: {
       CAT_API_KEY:
         "live_vn7IeMOozA5lOtBShVzNUkyPPV2TKqyFfuwXPHkaSHBNKhxMxp2P432UJCXaBwOf",
     },
-    setupNodeEvents(on, config) {
+    async setupNodeEvents(on, config) {
       addMatchImageSnapshotPlugin(on, config);
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin.default(config)],
+        })
+      );
+
       // Register the custom log task
       on("task", {
         log(message) {
--- cypress/e2e/step_definitions/source-regression-steps.js
+++ cypress/e2e/step_definitions/source-regression-steps.js
@@ -1,13 +1,9 @@
 /// <reference types="cypress" />
 
 import { Given, When, Then, And } from "@badeball/cypress-cucumber-preprocessor";
-import nbsHomePage from "../../support/page-objects/nbs-homepage.js";
-import dysonManufactureHomepage from "../../support/page-objects/dyson-manufacture-homepage.js";
-import commonObjects from "../../support/page-objects/common-objects.js";
-
-const dysonManufacturePage = new dysonManufactureHomepage();
-const nbsHome = new nbsHomePage();
-const common = new commonObjects();
+import nbsHome from "../../support/page-objects/nbs-homepage.js";
+import dysonManufacturePage from "../../support/page-objects/dyson-manufacture-homepage.js";
+import common from "../../support/page-objects/common-objects.js";
 
 // Background steps
 Given("I have navigated to the Dyson Manufacturer Homepage", () => {
